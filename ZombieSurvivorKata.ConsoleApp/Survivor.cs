c207cec12b542748d75ee31e9381fa8688a10ae5
adam watson <adamwatson08@gmail.com>
2023-01-29 18:45:51 -0600

    working on userInput

--- ZombieSurvivorKata.ConsoleApp/Survivor.cs
+++ ZombieSurvivorKata.ConsoleApp/Survivor.cs
@@ -8,12 +8,17 @@ public class Survivor
 	public int _wounds { get; set; }
 	public int _actionsPerTurn { get; set; }
 	public bool _alive { get; set; }
+	public List<string> _inHandEquipment { get; set; }
+	public List<string> _reserveEquipment { get; set; }
+
 	public Survivor(string name)
 	{
 		_name= name;
 		_wounds = 0;
 		_actionsPerTurn = 3;
 		_alive= true;
+		_inHandEquipment = new List<string>();
+		_reserveEquipment = new List<string>();
 	}
 
 	public void RecieveWound()
@@ -27,6 +32,27 @@ public class Survivor
 	public void Die()
 	{
 		_alive = false;
+	}
+
+	public void AddEquipment(string newEquipment)
+	{
+		var allEquipment = new List<string>();
+		allEquipment.AddRange(_inHandEquipment); 
+		allEquipment.AddRange(_reserveEquipment);
+		if(allEquipment.Count >= 5)
+		{
+			Console.WriteLine("You currently have the max equipment");
+			Console.WriteLine("Would you like to drop a piece of equipment for this one?");
+
+		}
+	}
+
+	public void PrintCurrentEquipment(List<string> allEquipment)
+	{
+		for (int i = 0; i < allEquipment.Count; i++)
+		{
+			Console.WriteLine(i + ": " + allEquipment[i]);
+		}
 	}
 
 }
