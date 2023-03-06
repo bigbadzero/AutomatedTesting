8aeec871a8068780dec917f9b85efb44e74788ac
Benjamin Watson <benjamin.watson@cgi.com>
2023-03-06 01:08:23 -0600

    removed rules

--- ZombieSurvivorKatana.Console/Rules/InHandRules/IInHandRules.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ZombieSurvivorKatana.ConsoleApp.Rules.InHandRules;
-
-public interface IInHandRules
-{
-    int Priority { get; }
-    public bool IsRuleApplicable(InHandEvent inHandEvent);
-    public void ExecuteRule(InHandEvent inHandEvent);
-}
