4d6771c3f52d999e6410724912cbc02c780fb08d
adam watson <adamwatson08@gmail.com>
2023-05-08 21:33:51 -0500

    first initial test compelte for FileLogger

---
+++ FileLoggerKata.Console/FileLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLoggerKata.Console
+{
+    public class FileLogger
+    {
+        public void Log(string message)
+        {
+            var logDate = DateTime.Now.ToString("yyyyMMdd");
+            string filePath = $"G:\\projects\\AutomatedTesting\\FileLoggerKata.Console\\log{logDate}.txt";
+            var date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            bool fileExists = File.Exists(message);
+            var log = $"{date} {message}";
+            if (fileExists)
+                File.AppendAllText(filePath, log);
+            
+            else
+            {
+                using (FileStream fs = File.Create(filePath))
+                {
+                    using (StreamWriter writer = new StreamWriter(fs))
+                    {
+                        writer.WriteLine(log);
+                        writer.Close();
+                    }
+                    fs.Close();
+                }
+
+            }
+        }
+    }
+}
---
+++ FileLoggerKata.Tests/FileLoggerTests.cs
@@ -0,0 +1,52 @@
+﻿using FileLoggerKata.Console;
+using Shouldly;
+
+namespace FileLoggerKata.Tests;
+
+public class FileLoggerTests
+{
+    [Fact]
+    public void Log_LogsText_ToLogtxtFile()
+    {
+        var test = "test";
+        var logDate = DateTime.Now.ToString("yyyyMMdd");
+        string fileLocation = $"G:\\projects\\AutomatedTesting\\FileLoggerKata.Console\\log{logDate}.txt";
+        File.Delete(fileLocation);
+        var logger = new FileLogger();
+
+        logger.Log(test);
+        var fileContents = File.ReadAllText(fileLocation);
+
+        fileContents.ShouldNotBeEmpty();
+    }
+
+    [Fact]
+    public void Log_Should_Prepend_Timestamp_To_Message_In_File()
+    {
+        var logDate = DateTime.Now.ToString("yyyyMMdd");
+        string filePath = $"G:\\projects\\AutomatedTesting\\FileLoggerKata.Console\\log{logDate}.txt";
+        var logger = new FileLogger();
+        File.Delete(filePath);
+
+        logger.Log("Test message");
+        var lines = File.ReadAllLines(filePath);
+
+        var lastLine = lines[lines.Length - 1];
+        var expectedPrefix = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        lastLine.ShouldStartWith(expectedPrefix);
+    }
+
+    [Fact]
+    public void Log_ShouldCreateLogFile_WithCorrectNamingConvention()
+    {
+        var logDate = DateTime.Now.ToString("yyyyMMdd");
+        string filePath = $"G:\\projects\\AutomatedTesting\\FileLoggerKata.Console\\log{logDate}.txt";
+        File.Delete(filePath);
+
+        var logger = new FileLogger();
+        logger.Log("Test message");
+        var result = File.Exists(filePath);
+
+        result.ShouldBeTrue();
+    }
+}
