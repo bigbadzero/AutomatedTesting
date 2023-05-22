8d702ee8fe14d5963dd4035d013e52cc2fc3726c
adam watson <adamwatson08@gmail.com>
2023-05-21 23:16:23 -0500

    removed unneeded files

--- RPGTestKatana/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using RPGTestKatana;
-
-Console.WriteLine("Hello, World!");
-var char1 = new RPGCombat.Character();
-var char2 = new RPGCombat.Character();
-
-char1.Attack(char2);
